988335fc8959d602303415324ddcf0c651749153
Kelly Adams <kelly@intellitect.com>
2013-12-10 08:45:53 -0800

    Adding a Win7 variant of Get-Disk that formats some WMI objects to show drive size and freespace.

---
+++ Functions.Tests/Get-Disk.Tests.ps1
@@ -0,0 +1,30 @@
+﻿$here = $PSScriptRoot
+$sut = $PSCommandPath.Replace(".Tests", "")
+. $sut
+
+Describe "Get-Disk" {
+    It "has a FormatStartData" {
+        $target = Get-Disk
+        $target[0].GetType().Name | Should Be "FormatStartData"
+    }
+
+    It "has a GroupStartData" {
+        $target = Get-Disk
+        $target[1].GetType().Name | Should Be "GroupStartData"
+    }
+
+    It "has a FormatEntryData" {
+        $target = Get-Disk
+        $target[2].GetType().Name | Should Be "FormatEntryData"
+    }
+
+    It "has a GroupEndData" {
+        $target = Get-Disk
+        $target[5].GetType().Name | Should Be "GroupEndData"
+    }
+
+    It "has a FormatEndData" {
+        $target = Get-Disk
+        $target[6].GetType().Name | Should Be "FormatEndData"
+    }
+}
---
+++ Functions/Get-Disk.ps1
@@ -0,0 +1,14 @@
+﻿If(!(get-module Storage -ListAvailable)) {
+    function Get-Disk {
+        Get-wmiObject -class "Win32_LogicalDisk" -namespace "root\CIMV2" -computername localhost `
+            | Select  DeviceID, `
+                      VolumeName, `
+                      Description, `
+                      FileSystem, `
+                      @{Name="SizeGB";Expression={($_.Size / 1GB).ToString("f3")}}, `
+                      @{Name="FreeGB";Expression={($_.FreeSpace / 1GB).ToString("f3")}} `
+            | Format-Table -AutoSize
+    }
+}
+
+Set-Alias df Get-Disk
