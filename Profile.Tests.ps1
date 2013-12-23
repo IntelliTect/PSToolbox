60489cb18b9294f982e54aff9b251c53d8632275
Mark Michaelis <mark@intellitect.com>
2013-12-23 08:34:24 -0800

    Removed alias command.

--- Profile.Tests.ps1
+++ Profile.Tests.ps1
@@ -3,10 +3,4 @@ $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests", "")
 . $sut
 
 
-Remove-Item alias:dir
-Remove-Item alias:ls
-Set-Alias dir Get-DirWithSize
-Set-Alias ls Get-DirWithSize
-
-
 
