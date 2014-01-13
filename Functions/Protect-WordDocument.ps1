2f156e23829c09dd0c4b78cf702bff9cbf5866f0
Mark Michaelis <mark@intellitect.com>
2014-01-13 07:28:26 -0800

    Add the start of a function that changes restrictions on word documents.

---
+++ Functions/Protect-WordDocument.ps1
@@ -0,0 +1,17 @@
+param(
+    $path,
+    $protectionType,
+    $password
+)
+
+$wordApplication = new-object -ComObject Word.Application
+
+$protectionType = [Microsoft.Office.Interop.Word.WdProtectionType]$protectionType
+
+$doc = $wordApplication.Documents.Open($path)
+
+$doc.Protect( $protectionType, [ref]$false, [ref]$password, [ref]$false, [ref]$false)
+
+$doc.Close()
+
+$wordApplication.Quit()
