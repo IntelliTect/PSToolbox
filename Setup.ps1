4351171892b3ec97168fd9075ac082c197789f1b
Mark Michaelis <mark@intellitect.com>
2014-01-17 08:39:56 -0800

    Updated Nuget Build to create Nuget package

--- Setup.ps1
+++ Setup.ps1
@@ -76,17 +76,16 @@ If(Test-Path variable:\psise) {
 Set-Alias Nuget "$ENV:ChocolateyInstall\chocolateyInstall\NuGet.exe" -Scope Global
 
 Function Build() {
-    [String] $ToolsDirectoryPath = (Join-Path $PSScriptRoot "Tools")
-    if(Test-Path $ToolsDirectoryPath) {
-        New-Item $ToolsDirectoryPath -ItemType Directory
-    }
-
     #Remove-Item (Join-Path $PSScriptRoot "\..\Tools") -Recurse
     #Copy-Item $PSScriptRoot $PSScriptRoot\..\Tools -Exclude "Tools" -Recurse -Force
     #Move-Item $PSScriptRoot\..\Tools $PSScriptRoot\Tools -WhatIf
     #TODO: Switc to use Copy-Item rather than Robocopy (good luck). :)
-    Robocopy $PSScriptRoot $PSScriptRoot\..\Tools /S /XC /MIR bin
-    RoboCopy $PSScriptRoot\..\Tools $PSScriptRoot\bin\Tools /s /MIR /MOVE
+    Robocopy $PSScriptRoot $PSScriptRoot\..\Tools * /S /XC /MIR /XD bin
+    if(!(Test-Path $PSScriptRoot\bin)) {
+        New-Item "$PSScriptRoot\bin" -ItemType Directory
+    }
+    Remove-Item $PSScriptRoot\bin\Tools -Recurse
+    Move $PSScriptRoot\..\Tools $PSScriptRoot\bin\Tools
     $currentDirectory = Get-Location;
     try {
         Set-Location $PSScriptRoot\bin\Tools
