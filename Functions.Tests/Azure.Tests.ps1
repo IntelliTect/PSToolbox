bb4efd22ee513c25d4339b14e93cb7fd65bda659
Dan Haley <dan.haley@IntelliTect.com>
2016-05-16 15:06:43 -0700

    Add New-AzureRmVirtualMachine

--- Functions.Tests/Azure.Tests.ps1
+++ Functions.Tests/Azure.Tests.ps1
@@ -1,3 +1,163 @@
-$here = $PSScriptRoot
+# Load the script we are going to test
 $sut = $PSCommandPath.Replace('.Tests', '')
 . $sut
+
+function CreateMocks {
+    Mock Get-AzureRmResourceGroup { return @{ ResourceGroupName = 'My Resource Group' } }
+    Mock New-AzureRmResourceGroup {}
+    Mock Get-AzureRmStorageAccount { return @{ 
+        StorageAccountName = 'My Storage Account'
+        PrimaryEndpoints = @{ 
+            Blob = "Blob URL" 
+        }}}
+    Mock New-AzureRmStorageAccount {}
+    Mock Get-AzureRmVirtualNetwork { return @{ 
+        Name = 'My Virtual Network'
+        Subnets = @(
+            @{ Id = 1 },
+            @{ Id = 2 }
+        )
+    }}
+    Mock New-AzureRmVirtualNetwork {}
+    Mock New-AzureRmVirtualNetworkSubnetConfig { 
+        param($Name) 
+        $ret = New-Object 'System.Collections.Generic.List[Microsoft.Azure.Commands.Network.Models.PSSubnet]'
+        $ret.Add(@{ Name = $Name })
+    }
+    Mock New-AzureRmNetworkInterface { param($Name) return @{ 
+        Id = 1  
+        Name = $Name 
+    }}
+    Mock New-AzureRmPublicIpAddress { param($Name) return @{ 
+        Id = 1
+        Name = $Name 
+    }}        
+    Mock New-AzureRmVMConfig { return @{} }
+    Mock Set-AzureRmVMOperatingSystem { return @{} }
+    Mock Set-AzureRmVMSourceImage { return @{} }
+    Mock Add-AzureRmVMNetworkInterface { return @{} }
+    Mock Set-AzureRmVMOSDisk { return @{} }
+    Mock New-AzureRmVM {}
+
+    Mock Test-AzureRmDnsAvailability { return $false }
+
+    Mock Get-AzureRmNetworkSecurityGroup { return @{ Id = 1 } }
+    Mock New-AzureRMNetworkSecurityGroup { return @{ Id = 1 } }
+    Mock Add-AzureRmNetworkSecurityRuleConfig {}
+    Mock Set-AzureRmNetworkSecurityGroup {}
+}
+
+Describe "New-AzureRMVirtualMachine Unit Tests" {
+    Context "All supporting Azure items are created new" {
+        CreateMocks
+
+        $newRG = 'My New Resource Group'
+        $newSA = 'My New Storage Account'
+        $newVN = 'My New Virtual Network'
+        
+        $result = New-AzureRMVirtualMachine -ResourceGroupName $newRG -VMName 'MyNewVM' -StorageAccountName $newSA -VirtualNetworkName $newVN -ImageSku '2012-Datacenter'
+         
+        It "Creates a resource group" {
+            Assert-MockCalled New-AzureRmResourceGroup -Times 1 -ParameterFilter { $Name -eq $newRG }
+        }
+
+        It "Creates a storage account" {
+            Assert-MockCalled New-AzureRmStorageAccount -Times 1 -ParameterFilter { $Name -eq $newSA }
+        }
+        
+        It "Creates a virtual network" {
+            Assert-MockCalled New-AzureRmVirtualNetwork -Times 1 -ParameterFilter { $Name -eq $newVN }
+        }
+        
+        It "Creates a public IP" {
+            Assert-MockCalled New-AzureRmPublicIpAddress -Times 1
+        }
+        
+        It "Creates a network interface" {
+            Assert-MockCalled New-AzureRmNetworkInterface -Times 1
+        }
+        
+        It "Succeeds with valid parameters" {
+            $result.Success | Should Be $true
+        }
+    }
+
+    Context "All supporting Azure items already exist" {
+        CreateMocks
+
+        $newRG = 'My Resource Group'
+        $newSA = 'My Storage Account'
+        $newVN = 'My Virtual Network'
+        $newDomain = 'existingdomain'
+
+        $result = New-AzureRMVirtualMachine -ResourceGroupName $newRG -VMName 'My New VM' -StorageAccountName $newSA -VirtualNetworkName $newVN -DomainNameLabel $newDomain -ImageSku '2012-Datacenter'
+
+        It "Fails when an existing domain name label is provided" {
+            $result.Success | Should Be $false
+        }
+        
+        $result = New-AzureRMVirtualMachine -ResourceGroupName $newRG -VMName 'My New VM' -StorageAccountName $newSA -ImageSku '2012-Datacenter'
+        
+        It "Does not create a resource group" {
+            Assert-MockCalled New-AzureRmResourceGroup -Times 0 -ParameterFilter { $Name -eq $newRG }
+        }
+
+        It "Does not create a storage account" {
+            Assert-MockCalled New-AzureRmStorageAccount -Times 0 -ParameterFilter { $Name -eq $newSA }
+        }
+        
+        It "Does not create a virtual network" {
+            Assert-MockCalled New-AzureRmVirtualNetwork -Times 0 -ParameterFilter { $Name -eq $newVN }
+        }
+        
+        It "Succeeds with valid parameters" {
+            $result.Success | Should Be $true
+        }
+    }
+}
+
+$functionalTest = @{
+    ResourceGroupName = "danhaleyvm2"
+    VMName = "danhaley2"
+    ImageSku = "2012-Datacenter"
+    StorageAccountName = ""
+    VMLocation = "West US"
+    StorageAccountType = "Standard_LRS"
+    DomainNameLabel = ""
+    VirtualNetworkName = ""
+    VMSize = "Standard_DS1_v2"
+    AdminCredentials = (New-Object PSCredential("vmadmin", ("P@ssword1!" | ConvertTo-SecureString -AsPlainText -Force)))
+}
+
+Describe "New-AzureRMVirtualMachine Functional Test" {
+    Context "Functional test" {
+        if ($functionalTest.ResourceGroupName -eq "")
+        {
+            Write-Information -MessageData "Test is inconclusive" -InformationAction Continue
+            Set-TestInconclusive "Functional test to create a VM has not been run.  Specify resource group, VM name, and image SKU to run functional test."
+        }
+        else 
+        {
+            Write-Information -MessageData "Creating a VM - this will take some time" -InformationAction Continue
+            $result = New-AzureRMVirtualMachine `
+                        -ResourceGroupName $functionalTest.ResourceGroupName `
+                        -VMName $functionalTest.VMName `
+                        -StorageAccountName $functionalTest.StorageAccountName `
+                        -ImageSku $functionalTest.ImageSku `
+                        -VMLocation $functionalTest.VMLocation `
+                        -StorageAccountType $functionalTest.StorageAccountType `
+                        -DomainNameLabel $functionalTest.DomainNameLabel `
+                        -VirtualNetworkName $functionalTest.VirtualNetworkName `
+                        -VMSize $functionalTest.VMSize `
+                        -AdminCredentials $functionalTest.AdminCredentials
+
+            It "Creates a VM on Azure" {
+                $result.Success | Should Be $true
+            }
+
+            Write-Information -MessageData "Removing test resource group" -InformationAction Continue
+            Remove-AzureRmResourceGroup -Name $functionalTest.ResourceGroupName -Force
+        }
+    }
+}
+
--- Functions/Azure.ps1
+++ Functions/Azure.ps1
@@ -1,28 +1,41 @@
-  Function Setup-AzureConfiguaration {
+function Ensure-AzureSession {
+    $Error.Clear();
+    $currentErrorAction = $ErrorActionPreference
+    $ErrorActionPreference = "SilentlyContinue"
+    Get-AzureRmContext | Out-Null
+    foreach ($ex in $Error) {
+        if ($ex.Exception.ToString() -like "*Run Login-AzureRmAccount to login.*") {
+            Login-AzureRmAccount | Out-Null
+        }
+    }
+    $Error.Clear()
+    $ErrorActionPreference = $currentErrorAction
+}
+
+Function Setup-AzureConfiguaration {
     [CmdletBinding(ConfirmImpact="High")]
-    param(
-           [pscredential]$azureCredentials = (Get-Credential -Message "Enter your azure credentials here.")
-        )
+    param (
+		[pscredential]$azureCredentials = (Get-Credential -Message "Enter your azure credentials here.")
+    )
 
-       if(!$PSCmdlet.ShouldProcess("Install-AzureConfig",“Confirm to execute", "3")) {
-            return
-       }
+    if (!$PSCmdlet.ShouldProcess("Install-AzureConfig", "Confirm to execute", "3")) {
+        return
+    }
 
-         
-        # Requires PSGet
-        Install-Module AzureRM        
-        Install-AzureRM
-        Install-Module Azure
-        Import-Module AzureRM
-        Install-AzureRM
+    # Requires PSGet
+    Install-Module AzureRM        
+    Install-AzureRM
+    Install-Module Azure
+    Import-Module AzureRM
+    Install-AzureRM
 
-        Write-Warning "Prompts for Azure Credentials"
-        Add-AzureAccount
+    Write-Warning "Prompts for Azure Credentials"
+    Add-AzureAccount
         
-        Write-Warning "Prompts to download PublishSettings file"
-        Get-AzurePublishSettingsFile
-        Get-ChildItem "$env:USERPROFILE\Downloads\" "*.publishsettings" | 
-            %{ Import-AzurePublishSettingsFile $_.FullName }
+    Write-Warning "Prompts to download PublishSettings file"
+    Get-AzurePublishSettingsFile
+    Get-ChildItem "$env:USERPROFILE\Downloads\" "*.publishsettings" | 
+        %{ Import-AzurePublishSettingsFile $_.FullName }
 }
 
 Function Initialize-Azure {
@@ -31,12 +44,11 @@ Function Initialize-Azure {
         [string]$subscriptionName
     )
 
-
-    "Azure","AzureRM" | %{ 
+    "Azure","AzureRM" | % { 
         $module = Get-Module $_ -ListAvailable
-        if(!$module) {
-            Write-Warning "The Azure module is not installed.  To install run 'Choco Install WindowsAzurePowershell'"
-            return
+        if (!$module) {
+			Write-Warning "The Azure module is not installed.  To install run 'Choco Install WindowsAzurePowershell'"
+			return
         }
         Write-Output $module
     } | Import-Module
@@ -44,21 +56,169 @@ Function Initialize-Azure {
     Set-AzureSubscription -SubscriptionName $subscriptionName
 }
 
+function New-AzureRmVirtualMachine {
+    [CmdletBinding()]    
+	param (
+        [Parameter(Mandatory)]
+        [string]$ResourceGroupName,
+        
+        [Parameter(Mandatory)]
+        [string]$VMName,
+      
+		[Parameter(Mandatory)]
+		[ValidateSet('2012-Datacenter', '2012-R2-Datacenter')]
+		$ImageSku,
+        
+        [string]$StorageAccountName = "",
+        [string]$VMLocation = "West US",
+        [string]$StorageAccountType = "Standard_LRS",
+        [string]$DomainNameLabel = "",
+        [string]$VirtualNetworkName = "",
+        [string]$VMSize = "Standard_DS1_v2",
+        [string]$NetworkSecurityGroup = "",
+        [PSCredential]$AdminCredentials = (New-Object PSCredential("vmadmin", ("P@ssword1!" | ConvertTo-SecureString -AsPlainText -Force)))
+	)
+
+    $ErrorActionPreference = "Stop"
+    Set-StrictMode -Version Latest
+
+    Ensure-AzureSession
+    
+    # Defaults
+    if ($VirtualNetworkName -eq "")
+    {
+        $VirtualNetworkName = $ResourceGroupName
+    }
+
+    if ($StorageAccountName -eq "")
+    {
+        $StorageAccountName = $ResourceGroupName
+    }
+
+    # Choices
+    if ($VMSize -eq "choose") {
+        $selectedSizes = Get-AzureRmVmSize -Location $VMLocation | Out-GridView -OutputMode Single
+        if ($selectedSizes -eq $null) {
+            throw 'When $VMSize == choose, you must choose a size to continue.'
+        } else {
+            $VMSize = $selectedSizes[0].Name
+        }
+    }
+
+    # If the resource group doesn't exist, then create it
+    $groups = Get-AzureRmResourceGroup | ? { $_.ResourceGroupName -eq $ResourceGroupName }
+    if ($groups -eq $null)
+    {
+        Write-Information -MessageData "Creating new resource group" -InformationAction Continue
+        $ignore1 = New-AzureRmResourceGroup -Name $ResourceGroupName -Location $VMLocation
+    }
+    
+    # If the storage account doesn't exist, then create it
+    $accounts = Get-AzureRmStorageAccount | ? { $_.StorageAccountName -eq $StorageAccountName }
+    if ($accounts -eq $null)
+    {
+        Write-Information -MessageData "Creating new storage account" -InformationAction Continue
+        $ignore1 = New-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroupName –Type $StorageAccountType -Location $VMLocation
+    }
+    
+    # If the virtual network doesn't exist, then create it
+    $vns = Get-AzureRmVirtualNetwork | ? { $_.Name -eq $VirtualNetworkName }
+    if ($vns -eq $null)
+    {
+        Write-Information -MessageData "Creating new virtual network" -InformationAction Continue
+        $defaultSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name "defaultSubnet" -AddressPrefix "10.0.2.0/24"
+        $ignore1 = New-AzureRmVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName -Location $VMLocation -AddressPrefix "10.0.0.0/16" -Subnet $defaultSubnet
+    }
+    
+    # If a domain name label is supplied, then test that it isn't in use
+    if ($DomainNameLabel -ne "")
+    {
+        $domainOk = Test-AzureRmDnsAvailability -DomainQualifiedName $DomainNameLabel -Location $VMLocation
+        if ($domainOk -eq $false)
+        {
+            $message = "DomainNameLabel ($DomainNameLabel) failed when tested for uniqueness."
+            Write-Information -MessageData $message -InformationAction Continue
+            return @{
+                Success = $false
+                Message = $message
+            }
+        }
+    }
+    
+    # Create, if needed, the network security group and add a default RDP rule
+    $nsg = $null
+    $securityGroupName = @{$true = $ResourceGroupName; $false = $NetworkSecurityGroup}[$NetworkSecurityGroup -eq '']
+    if ($NetworkSecurityGroup -ne "")
+    {
+        $nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName $ResourceGroupName | ? { $_.Name -eq $securityGroupName }   
+    }
+    if ($nsg -eq $null)
+    {
+        Write-Information -MessageData "Creating security group and rule" -InformationAction Continue
+        $nsg = New-AzureRMNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Name $securityGroupName -Location $VMLocation
+        $nsg | Add-AzureRmNetworkSecurityRuleConfig -Name 'Allow_RDP' -Priority 1000 -Protocol TCP -Access Allow -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Direction Inbound | Set-AzureRmNetworkSecurityGroup | Out-Null
+    }
+
+    # Create the public IP and NIC
+    $vnet = Get-AzureRmVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName
+    $ticks = (Get-Date).Ticks.ToString()
+    $ticks = $ticks.Substring($ticks.Length - 5, 5)
+    $nicName = "$ResourceGroupName$ticks"
+    if ($DomainNameLabel -ne "")
+    {
+        Write-Information -MessageData "Creating new public IP address with domain name" -InformationAction Continue
+        $pip = New-AzureRmPublicIpAddress -Name $nicName -ResourceGroupName $ResourceGroupName -DomainNameLabel $DomainNameLabel -Location $VMLocation -AllocationMethod Dynamic
+    }
+    else
+    {
+        Write-Information -MessageData "Creating new public IP address WITHOUT domain name" -InformationAction Continue
+        $pip = New-AzureRmPublicIpAddress -Name $nicName -ResourceGroupName $ResourceGroupName -Location $VMLocation -AllocationMethod Dynamic
+    }
+    Write-Information -MessageData "Creating new network interface" -InformationAction Continue
+    $nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $ResourceGroupName -Location $VMLocation -PublicIpAddressId $pip.Id -SubnetId $vnet.Subnets[0].Id -NetworkSecurityGroupId $nsg.Id
+    
+    # Create the VM configuration
+    Write-Information -MessageData "Creating VM configuration" -InformationAction Continue
+    $vm = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+    
+    # Add additional info to the configuration
+    $vm = Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $VMName -Credential $AdminCredentials -ProvisionVMAgent -EnableAutoUpdate
+    $vm = Set-AzureRmVMSourceImage -VM $vm -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus $ImageSku -Version "latest"
+    $vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+
+    # Create the disk
+    $diskName = "$($VMName)OSDisk"
+    $storage = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
+    $osDisk = $storage.PrimaryEndpoints.Blob.ToString() + "vhds/" + $diskName  + ".vhd"
+    $vm = Set-AzureRmVMOSDisk -VM $vm -Name $diskName -VhdUri $osDisk -CreateOption fromImage
+
+    # And finally create the VM itself
+    Write-Information -MessageData "Creating the VM ... this will take some time ..." -InformationAction Continue
+    New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $VMLocation -VM $vm
+
+    return @{
+        Success = $true
+        Message = "VM created successfully"
+    }
+}
 
 Function New-AzureVM {
-    [CmdletBinding()] Param(
-        [Parameter(Mandatory)][string]$imageName
-        , [Parameter(Mandatory)][string] $newName
-        , [PSCredential] $credential
-        , [string] $instanceSize = "Small"
-        , [string] $location = "West US"
+    [CmdletBinding()] 
+	Param(
+        [Parameter(Mandatory)]
+		[string]$imageName,
+        [Parameter(Mandatory)]
+		[string] $newName,
+		[PSCredential] $credential,
+		[string] $instanceSize = "Small",
+        [string] $location = "West US"
         #[OSImageContext] $image
     )
 
     $vmConfig = New-AzureVMConfig -Name $newName -InstanceSize $instanceSize -Image $imageName |
         Add-AzureProvisioningConfig -Windows -AdminUserName $credential.UserName -Password (Get-CredentialPassword $credential) |
         . Azure\New-AzureVM -ServiceName $newName -Location $location
-} 
+}
 
 
 Function Enter-AzurePSSEssion {
