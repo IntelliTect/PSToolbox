33a157fde2e5514539654ae7f24e45b6c6652b88
Andrew Scott <ascott18@gmail.com>
2016-05-24 14:34:36 -0700

    Removed old, unused files.

--- Build.ps1
+++
@@ -1,2 +0,0 @@
-Import-Module (Join-Path $PSScriptRoot "PSIdeation.psm1") -Verbose -Scope Local
-New-NugetPackage -inputDirectory $PSScriptRoot -outputDirectory (Join-Path $PSScriptRoot bin)
--- PSIdeation.psm1
+++
@@ -1,1 +0,0 @@
-. (Join-Path $PSScriptRoot "Main.ps1")
