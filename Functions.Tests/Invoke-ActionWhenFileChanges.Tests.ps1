3a2769c6fc58de697adc7b8aebb9bc87607b2d33
Mark Michaelis <Mark Michaelis>
2016-07-27 09:01:15 -0700

    Added TO DO list.

--- Functions.Tests/Invoke-ActionWhenFileChanges.Tests.ps1
+++ Functions.Tests/Invoke-ActionWhenFileChanges.Tests.ps1
@@ -2,7 +2,10 @@ $here = $PSScriptRoot
 $sut = $PSCommandPath.Replace('.Tests', '')
 . $sut
 
-
+<# TO DO
+    1. Test file needs to be clean up.
+    2. Currently tests fail because they are not asynchronous.
+#>
 
 Describe 'Invoke-ActionWhenFileChanges' {
     It 'Touch file fires event' {
