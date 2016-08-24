81f710dec65750f6ded4f51b5c8a10072415ce3e
Kelly Adams <kelly@intellitect.com>
2016-08-24 09:14:37 -0700

    Clean up test and use Register-AutoDispose.

--- Functions.Tests/Invoke-ActionWhenFileChanges.Tests.ps1
+++ Functions.Tests/Invoke-ActionWhenFileChanges.Tests.ps1
@@ -3,37 +3,24 @@ $sut = $PSCommandPath.Replace('.Tests', '')
 . (Join-Path (Split-Path $sut -Parent) "Common.ps1")
 . $sut
 
-<# TO DO
-    1. Test file needs to be clean up.
-    2. Currently tests fail because they are not asynchronous.
-#>
-
-# TODO: Move this into File.ps1 and include (dot-source) into Common.ps1
 Function Set-FileTime
-
 {
-
  Param (
-
     [Parameter(mandatory=$true)]
-
     [string[]]$path,
-
     [datetime]$date = (Get-Date))
-
+    
     Get-ChildItem -Path $path |
-
     ForEach-Object {
-
      $_.LastAccessTime = $date
-
-     $_.LastWriteTime = $date }
-
+     $_.LastWriteTime = $date 
+    }
 }
 
 Describe 'Invoke-ActionWhenFileChanges' {
     It 'Touch file fires event' {
         $sampleFile = Get-TempFile
+        Register-AutoDispose $sampleFile {
             
             $script:isCalled = $false
             $aScript = { $script:isCalled = $true }
@@ -44,8 +31,7 @@ Describe 'Invoke-ActionWhenFileChanges' {
             Start-Sleep -Seconds 1
             Set-FileTime $sampleFile
             $script:isCalled | Should Be $true 
-
-            $sampleFile.Dispose()
             $ev.StopJob()
+        }
     }
 }
