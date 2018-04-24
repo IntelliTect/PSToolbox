65d458a504a76b96a7745cdfa57fd8ac6558d7b8
Mark Michaelis <mark@IntelliTect.com>
2018-04-23 17:06:27 -0700

    Documented TODOs for Set-Alias with functions and ShouldProcess

--- Modules/IntelliTect.Common/IntelliTect.Common.psm1
+++ Modules/IntelliTect.Common/IntelliTect.Common.psm1
@@ -6,7 +6,7 @@ param(
     [string]$EnvironmentVariableName = "Path",
     [EnvironmentVariableTarget]$Scope=[EnvironmentVariableTarget]::User
 )
-    
+
     if( ! (([Environment]::GetEnvironmentVariable($EnvironmentVariableName, $Scope) -split ';') -contains $Path) ) {
         if($PSCmdlet.ShouldProcess("Add '$Path' to '`$env:$EnvironmentVariableName'.")) {
             $CurrentValue = [Environment]::GetEnvironmentVariable($EnvironmentVariableName, $Scope)
@@ -15,3 +15,19 @@ param(
         }
     }
 }
+
+#TODO: Add Intelligent $PSISE.ShouldProcess wrapper
+<#
+Function Invoke-ShouldProcess?????{
+    $command = "git mv $oldFileName $newFileName $(if($PSBoundParameters['Verbose']) {`"-v`"})" # The following is not needed as it is handled by "$PSCmdlet.ShouldProcess": -What $(if($PSBoundParameters['WhatIf']) {`"--dry-run`"})"
+    if ($PSCmdlet.ShouldProcess("`tExecuting: $command", "`tExecute git.exe Rename: $command", "Executing Git.exe mv")) {
+        Invoke-Expression "$command" -ErrorAction Stop  #Change error handling to use throw instead.
+    }
+}
+Set-Alias ShouldProcess Invoke-ShouldProcess
+#>
+
+<#
+TODO: Create Set-Alias Wrapper that allows the passing of parameters
+
+#>
