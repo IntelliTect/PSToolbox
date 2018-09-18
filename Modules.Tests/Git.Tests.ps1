51943c12837e3e06c9feeefffd2ff3e75559aefc
Mark Michaelis <mark@IntelliTect.com>
2018-09-17 21:08:26 -0700

    Further progress on Undo-Git.

--- Modules.Tests/Git.Tests.ps1
+++ Modules.Tests/Git.Tests.ps1
@@ -9,7 +9,7 @@ Function Script:Initialize-TestGitRepo {
     $tempDirectory = Get-TempDirectory
     try {
         Push-Location $tempDirectory
-        Invoke-GitCommand 'Init'
+        Invoke-GitCommand -ActionMessage "Initialize a temporary repository in '$tempDirectory'." -Command 'git init'
     }
     finally {
         Pop-Location
@@ -18,5 +18,49 @@ Function Script:Initialize-TestGitRepo {
 }
 
 
+Describe "Get-GitStatusObject" {
+    (Script:Initialize-TestGitRepo) | Register-AutoDispose -ScriptBlock {
+        It "No file exists for the given name" {
+
+        }
+    }
+}
 
 
+
+
+Describe 'Get-GitObjectProperty' {
+    It 'Return all properties' {
+        'refname','parent','authorname' | ForEach-Object {
+            (Get-GitObjectProperty) -contains $_ | Should Be $true
+        }
+    }
+    It 'Return a specific property' {
+        'refname','parent','authorname' | ForEach-Object {
+            Get-GitObjectProperty -Name "$_" | Should Be "$_"
+        }
+    }
+    It 'Return propery based on wildcard (*) suffix' {
+        'object*' | ForEach-Object {
+            Get-GitObjectProperty -Name "$_" | Should Be 'objecttype','objectsize','objectname','object'
+        }
+    }
+    It 'Return propery based on wildcard (*) prefix' {
+        '*parent' | ForEach-Object {
+            Get-GitObjectProperty -Name "$_" | Should Be 'parent','numparent'
+        }
+    }
+    # It 'Return properties as Json format string.' {
+    #     'refname','refname:short','authorname' | ForEach-Object {
+    #         Get-GitObjectProperty -Name "$_" -Format 'Json' | Should Be "`"$_`":`"%($_)`""
+    #     }
+    # }
+    # It 'Return properties as git format strings.' {
+    #     'refname','refname:short','authorname' | ForEach-Object {
+    #         Get-GitObjectProperty -Name "$_" -Format 'GitFormat' | Should Be "%($_)"
+    #     }
+    # }
+    It 'Given a collection of names/wildcards, return only those' {
+            Get-GitObjectProperty -Name 'object*','authorname' | Should Be 'objecttype','objectsize','objectname','object','authorname'
+    }
+}
