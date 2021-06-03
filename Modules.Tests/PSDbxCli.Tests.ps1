72017dc54c161c2a9dc3c6be1f387f5abcf68dc6
Mark Michaelis <mark@IntelliTect.com>
2021-06-03 06:48:13 -0700

    Implemented Test-DbxPath

--- Modules.Tests/PSDbxCli.Tests.ps1
+++ Modules.Tests/PSDbxCli.Tests.ps1
@@ -8,6 +8,42 @@ Get-Module IntelliTect.PSDbxCli | Remove-Module
 Import-Module -Name $PSScriptRoot\..\Modules\IntelliTect.PSDbxCli -Force
 #EndHeader#>
 
+Describe 'Test-DbxPath' {
+    BeforeAll {
+        $rootFiles = Get-DbxItem -File
+        if(-not $rootFiles) { throw 'There are no exisint sample files at the root folder to test with.' }
+        $rootDirectories = Get-DbxItem -Directory
+        if(-not $rootFiles) { throw 'There are no exisint sample directories at the root folder to test with.' }
+        $script:sampleFileAtRootPath = $rootFiles[(Get-Random -Maximum ($rootFiles.Count-1))].Path
+        $script:sampleDirectoryAtRootPath = $rootDirectories[(Get-Random -Maximum ($rootDirectories.Count-1))].Path
+    }
+    It "Verify item (a file called '$script:sampleFileAtRootPath') exists" {
+        Test-DbxPath $script:sampleFileAtRootPath | Should -BeTrue
+    }
+    It "Verify item (a directory called '$script:sampleDirectoryAtRootPath') exists" {
+        Test-DbxPath $script:sampleDirectoryAtRootPath | Should -BeTrue
+    }
+    It "Verify a file (-File) called '$script:sampleFileAtRootPath' exists" {
+        Test-DbxPath -PathType Leaf $script:sampleFileAtRootPath | Should -BeTrue
+    }
+    It "Verify a directory (-Container) called '$script:sampleDirectoryAtRootPath' exists" {
+        Test-DbxPath -PathType Container $script:sampleDirectoryAtRootPath | Should -BeTrue
+    }
+    It "Verify a directory called '$($sampleDirectoryAtRootPath.TrimEnd('/')) (without trailing slash) exists" {
+        # Verify trailing slash is allowed
+        Test-DbxPath -PathType Container $sampleDirectoryAtRootPath.TrimEnd('/') | Should -BeTrue
+    }
+    It 'Verify slash prefix will be assumed.' {
+        Test-DbxPath -PathType Container $sampleDirectoryAtRootPath.TrimStart('/') | Should -BeTrue
+    }
+    It "Verify folder (called 'Bogus-Bogus-Bogus.bogus') does not exists" {
+        Test-DbxPath -PathType Container 'Bogus-Bogus-Bogus.bogus' | Should -BeFalse
+    }
+    It 'Verify searching for a file with a trailing ''/'' will throw an error' {
+        { Test-DbxPath -PathType Leaf '/Folder/' } | Should -Throw
+    }
+}
+
 Describe 'Get-DbxItem' {
     It 'Verify you can see the root' {
         $items = Get-DbxItem
