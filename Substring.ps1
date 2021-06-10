e00ab7edeee7ee5a163fe22d3bb3c142e404759d
Tyler Jones <tyler.jones@intellitect.com>
2021-06-10 15:01:13 -0700

    One commit

--- Substring.ps1
+++ Substring.ps1
@@ -1,3 +1,13 @@
-[string]$yes = "IntelliTect.PSToolbox.psd1"
-$yes = $yes.Substring(0, $yes.length - 5)
-$yes
+# [string]$yes = "IntelliTect.PSToolbox.psd1"
+# $yes = $yes.Substring(0, $yes.length - 5)
+# $yes
+
+$logs = git log --pretty=format:"%s"
+$commitNum = 1
+foreach ($log in $logs) {
+    if($log.Contains("[skip ci]")){
+        break
+    }
+    $commitNum++
+}
+$commitNum
